eaba5e6a7b7d955d2df4b69a05144d0dcfb461d9
Sergej Atamantschuk <sergatam@yandex.com>
2018-07-13 11:38:55 +0200

    added vuetifyjs

---
+++ client/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/login">Login</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
+
+<style lang="scss">
+  body, html{
+    height: 100%;
+      background-color: #f0f0f0;
+      #app {
+          main {
+          }
+      }
+  }
+
+.v-toolbar__content {
+  text-align: center;
+  a {
+    transition: all .3s;
+    font-weight: bold;
+    color: #2c3e50;
+    &:hover {
+      color: #62b9a9;
+    ;
+    }
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="d">
+
+        <div id="login">
+            <form action="">
+                <img src="img/ar.jpg"><br>
+                <input type="text" value="arinas" placeholder="Benutzername"><br>
+                <input type="password" placeholder="Passwort"><br>
+                <input type="submit" value="Einloggen"><br>
+                <span><a href="#">Passwort vergessen?</a></span>
+            </form>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data: () => ({
+            gradient: 'to top right, rgba(63,81,181, .7), rgba(25,32,72, .7)'
+        })
+    }
+</script>
+<style lang="scss" scoped>
+    .login-form-container {
+        /*padding: 20% 35%;*/
+        position: absolute;
+        display: flex;
+        width: 100%;
+        height: 100%;
+       margin: auto;
+        top: 0;
+        left: 0;
+        align-items: center;
+        background: url("../../public/back.png") no-repeat;
+        background-size: cover;
+
+        .login-form {
+            background: rgba(238, 238, 238, 0.51);
+            z-index: 100;
+            width: 100%;
+            .card-head{
+                display: flex;
+                justify-content: left;
+            }
+        }
+    }
+
+    #d{
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background: url("../../public/back.png") no-repeat;
+        background-size: cover;
+
+    }
+
+    #login{
+        width: 350px;
+        height: 500px;
+        background: inherit;
+        position: absolute;
+        overflow: hidden;
+        top: 50%;
+        left: 50%;
+        margin-left: -175px;
+        margin-top: -250px;
+        border-radius: 8px;
+    }
+    #login:before{
+        width: 400px;
+        height: 550px;
+        content: "";
+        position: absolute;
+        top: -25px;
+        left: -25px;
+        bottom: 0;
+        right: 0;
+        filter: blur(10px);
+        background: inherit;
+        box-shadow: inset 0 0 0 200px rgba(255,255,255,0.2);
+    }
+
+</style>
